d0d814ed71f99a2b96256ce25bda91d0e3dc4f91
mdtanjidkhan <mdtanjidkhan152@gmail.com>
2026-06-24 12:40:20 +0600

    feat(logo): integrate ImgBB image upload with responsive image preview

--- src/app/dashboard/overview/my-applications/explore-projects/page.jsx
+++ src/app/dashboard/overview/my-applications/explore-projects/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ExploreProjectPage = () => {
     return (
         <div>
-            explore-projects
+            explore-projects now 
         </div>
     );
 };
